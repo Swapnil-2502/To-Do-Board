c5f6714d37d8f38af9c36c69305858eb94179258
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-07-06 19:49:57 +0530

    Drag and Drop Done

--- frontend/src/pages/Board.tsx
+++ frontend/src/pages/Board.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./Board.css"
 import type { Task } from "../types";
-import { getTasks } from "../api/task";
+import { getTasks, updateTask } from "../api/task";
 
 const STATUSES: Task["status"][] = ["Todo", "In Progress", "Done"] as const;
 
@@ -21,6 +21,27 @@ export default function Board(){
         fetchTasks();
     },[])
 
+    const handleDrop = async (e: React.DragEvent, newStatus: Task["status"]) => {
+        const taskId = e.dataTransfer.getData('taskId')
+        const fromstatus = e.dataTransfer.getData('fromstatus')
+
+        if (fromstatus === newStatus) return;
+
+        const taskToUpdate = tasks.find((t) => t._id === taskId)
+        if (!taskToUpdate) return;
+
+        try{
+            await updateTask(taskId,{status:newStatus})
+            setTasks((prev)=>
+                prev.map((t) => (t._id === taskId ? {...t,status:newStatus}: t))
+            )
+        }
+        catch(error){
+            console.error("Failed to update task status", error);
+        }
+
+    }
+
   return (
     <>
     <div className="topbar">
@@ -31,11 +52,29 @@ export default function Board(){
         {STATUSES.map((status)=>(
             <div key={status} className="board-column">
                 <h3>{status}</h3>
-                <div className="tast-list" data-status={status}>
+                <div className="task-list" data-status={status}
+                    onDragOver={(e) => {
+                        e.preventDefault()
+                        e.currentTarget.classList.add("drag-over");
+                    } }
+                    onDragLeave={(e) => {
+                        e.currentTarget.classList.remove("drag-over");
+                    }}
+                    onDrop={(e) => {
+                        e.currentTarget.classList.remove("drag-over");
+                        handleDrop(e,status)
+                    } }
+                    >
                     {tasks
                         .filter((task) => task.status === status)
                         .map((task)=>(
-                            <div key={task._id} className="task-card">
+                            <div key={task._id} className="task-card" 
+                                draggable 
+                                onDragStart={(e)=>{
+                                    e.dataTransfer.setData('taskId',task._id)
+                                    e.dataTransfer.setData('fromstatus',task.status)
+                                }}
+                            >
                                 <div className="task-header">
                                     <h4>{task.title}</h4>
                                     <span className={`badge p${task.priority}`}>P{task.priority}</span>
